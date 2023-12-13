f62d5f1840e9c967eb8efa7a4473b6906680614e
osman armut <rogerfavelron@gmail.com>
2023-12-13 18:08:43 +0300

    feat: NextJS project setup

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rick and Morty",
+  description: "Rick and Morty app built with NextJS",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

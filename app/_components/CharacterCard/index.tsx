744c3823db1762de7d6ed7dc9288bff4a2c977fa
osman armut <rogerfavelron@gmail.com>
2023-12-15 03:24:46 +0300

    feat: character page with ssr and other characters

--- app/_components/CharacterCard/index.tsx
+++ app/_components/CharacterCard/index.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import cn from "classnames";
 import styles from "./characterCard.module.scss";
 
 import Status from "../Status";
@@ -24,11 +26,13 @@ const CharacterCard = ({
   image,
 }: CharacterCardType) => {
   return (
-    <div className={styles.card} id={String(id)}>
+    <div className={cn(styles.card, mini && styles.miniCard)}>
       <div className={styles.imageContainer}>
-        <button className={styles.heart}>
-          <Heart color="#ffffff" />
-        </button>
+        {!mini && (
+          <button className={styles.heart}>
+            <Heart color="#ffffff" />
+          </button>
+        )}
         <Image
           src={image}
           fill
@@ -36,17 +40,38 @@ const CharacterCard = ({
           sizes="(max-width: 768px) 300px, (max-width: 1200px) 400px"
         />
       </div>
-      <div className={styles.info}>
-        <div className={styles.left}>
-          <span className={styles.name}>{name}</span>
-          <span className={styles.statusContainer}>
-            <Status type={status} /> - {species}
-          </span>
+      <Link
+        href={`/characters/${id}`}
+        className={cn(fullDetail && styles.unclickable)}
+      >
+        <div className={styles.info}>
+          <div className={styles.left}>
+            <span className={styles.name}>{name}</span>
+            {!mini && (
+              <span className={styles.statusContainer}>
+                <Status type={status} /> - {species}
+              </span>
+            )}
+            {(fullDetail || mini) && (
+              <span className={styles.origin}>{origin.name}</span>
+            )}
+          </div>
+          <div
+            className={cn(
+              fullDetail && styles.detailRight,
+              mini && styles.miniRight
+            )}
+          >
+            {!fullDetail && !mini ? (
+              <RightArrow />
+            ) : (
+              <span>
+                {type || "No Type"} / {gender}
+              </span>
+            )}
+          </div>
         </div>
-        <div className={styles.right}>
-          <RightArrow />
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
---
+++ app/characters/[characterId]/page.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import shuffle from "lodash/shuffle";
+import styles from "./character.module.scss";
+
+import CharacterCard from "@/app/_components/CharacterCard";
+
+import type { CharacterApiResponseType } from "@/app/types";
+
+const Character = async ({ params }: { params: { characterId: string } }) => {
+  const characterId = Number(params.characterId);
+
+  const characterData = await fetch(
+    `${process.env.API_URL}/character/${characterId}`,
+    {
+      cache: "force-cache",
+    }
+  );
+  const characterResponse = await characterData.json();
+  const parts = characterResponse.location.url.split("/");
+  const locationId = Number(parts[parts.length - 1]);
+  const status = characterResponse.status;
+
+  //We need to find two other characters from the same location with same status
+  const locationData = await fetch(
+    `${process.env.API_URL}/location/${locationId}`,
+    {
+      cache: "force-cache",
+    }
+  );
+  const locationResponse = await locationData.json();
+
+  const allCharacterIds = locationResponse.residents.map((resident: string) => {
+    const parts = resident.split("/");
+    return Number(parts[parts.length - 1]);
+  });
+
+  //We need to fetch all characters to be able to filter by status
+  const charactersData = await fetch(
+    `${process.env.API_URL}/character/${allCharacterIds.join(",")}`,
+    {
+      cache: "force-cache",
+    }
+  );
+  const charactersResponse = await charactersData.json();
+
+  let filteredCharacters = charactersResponse.filter(
+    (c: CharacterApiResponseType) => c.status === status && c.id !== characterId
+  );
+
+  //Only get the first 2 for random
+  filteredCharacters = shuffle(filteredCharacters).slice(0, 2);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.main}>
+        <CharacterCard {...characterResponse} fullDetail />
+      </div>
+
+      {filteredCharacters.length > 0 && (
+        <div className={styles.others}>
+          <div className={styles.title}>Other Characters</div>
+          <div className={styles.miniCards}>
+            {filteredCharacters.map((c: CharacterApiResponseType) => (
+              <CharacterCard {...c} mini />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Character;
